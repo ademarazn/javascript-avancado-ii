fef0af240d9a42a56d686aa479933fa2e0e361c2
Ademar Zório Neto <ademarazn@hotmail.com>
2020-04-15 00:59:35 -0300

    conteudo do curso
    aula 3
    
    modificacao

--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -5,8 +5,10 @@ class ProxyFactory {
         if (props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
           return function() {
             console.log(`interceptando ${prop}`);
-            Reflect.apply(target[prop], target, arguments);
-            return acao(target);
+            let retorno = Reflect.apply(target[prop], target, arguments);
+            acao(target);
+
+            return retorno;
           }
         }
 
@@ -14,12 +16,9 @@ class ProxyFactory {
       },
 
       set(target, prop, value, receiver) {
-        if (props.includes(prop)) {
-          target[prop] = value;
-          acao(target);
-        }
-
-        return Reflect.set(target, prop, value, receiver);
+        let retorno = Reflect.set(target, prop, value, receiver);
+        if (props.includes(prop)) acao(target);
+        return retorno;
       }
     });
   }
